f704a130b0f133c36e037ec6b58cdf9096ff940c
nicholasmarson <nicholasmarson@Nicholass-MBP.attlocal.net>
2023-10-22 19:13:22 -0400

    Still working on deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 module.exports = {
+  publicPath: "/",
+  outDir: "docs",
   chainWebpack: (config) => {
     // Add a rule for handling PDF files
     config.module
